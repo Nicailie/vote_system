e60c295fd90cf8109df7b848b3b0f7c6958a63a5
Nice Cailie Ineza <91422726+Nicailie@users.noreply.github.com>
2023-04-06 14:50:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,64 +12,104 @@ db = firestore.client(app=new)
 app = flask.Flask(__name__)
 
 @app.route('/', methods=['POST', 'DELETE', 'PUT', 'GET', 'PATCH'])
-def main(request):
-    # Depending on the request method and path, call the respective functiton
+def main_try(request):
+    # Check if request method is allowed
+    if request.method not in ['POST', 'DELETE', 'PUT', 'GET', 'PATCH']:
+        return jsonify("Invalid request method"), 400
+
+    # Call the respective function based on the request method and path
     if request.method == 'POST' and request.path == '/voters':
         return register(request)
     elif request.method == 'PUT' and request.path == '/voters':
         return update(request)
-    elif request.method == 'GET' and request.path.startswith('/voters'):
-        return get_student(request)
+    elif request.method == 'GET' and request.path == '/voters':
+        return get_voter(request)
     elif request.method == 'DELETE' and request.path == '/voters':
         return delete(request)
-    elif request.method == 'POST' and request.path == '/elections':
-        return create_election(request)
-    elif request.method == 'GET' and  request.path.startswith('/elections'):
-        return get_election(request)
-    elif request.method == 'DELETE' and request.path == '/elections':
-        return delete_election(request)
     else:
-        return jsonify("Sorry, invalid request"), 400
+        return jsonify("Invalid request path"), 400
 
-def register(request): 
-    detail = request.get_json() 
-    voters_db = db.collection('voters')
+
+def register(request):
+    # Check if request body is valid JSON
+    try:
+        detail = request.get_json()
+    except ValueError:
+        return jsonify("Invalid request body"), 400
+
+    # Check if voter with the same ID already exists
     query = voters_db.where('id', '==', detail['id']).get()
     if len(query) > 0:
-        return jsonify("Record is already present"), 409
-    voters_db.add(detail)
+        return jsonify("Record with the same ID already exists"), 409
+
+    # Add voter to the database
+    try:
+        voters_db.add(detail)
+    except:
+        return jsonify("Failed to add record to the database"), 500
+
     return jsonify(detail), 201
 
+
 def update(request):
-    get_id = request.get_json()
-    voters_db = db.collection('voters')
+    # Check if request body is valid JSON
+    try:
+        get_id = request.get_json()
+    except ValueError:
+        return jsonify("Invalid request body"), 400
+
+    # Check if voter with the given ID exists
     query = voters_db.where('id', '==', get_id['id']).get()
     if len(query) == 0:
-        return jsonify(get_id), 404
-    for doc in query:
-        doc.reference.update(get_id)
-    return "The student_id was updated", 200
+        return jsonify("Record not found"), 404
 
-def get_student(request):
+    # Update the voter in the database
+    try:
+        for doc in query:
+            doc.reference.update(get_id)
+    except:
+        return jsonify("Failed to update record in the database"), 500
+
+    return jsonify(get_id), 200
+
+
+def get_voter(request):
+    # Get the voter ID from query parameter
     stud_id = request.args.get('id')
-    voters_db = db.collection('voters')
+
+    # Check if voter with the given ID exists
     query = voters_db.where('id', '==', stud_id).get()
     if len(query) == 0:
-        return jsonify({'error': "The student is not found"}), 404
+        return jsonify("Record not found"), 404
+
+    # Return the voter details
     for doc in query:
         return jsonify(doc.to_dict()), 200
 
+
 def delete(request):
-    del_id = request.get_json() 
-    voters_db = db.collection('voters')
+    # Check if request body is valid JSON
+    try:
+        del_id = request.get_json()
+    except ValueError:
+        return jsonify("Invalid request body"), 400
+
+    # Check if voter with the given ID exists
     query = voters_db.where('id', '==', del_id['id']).get()
     count = 0
     for doc in query:
-        doc.reference.delete()
-        count += 1
+        # Delete the voter from the database
+        try:
+            doc.reference.delete()
+            count += 1
+        except:
+            return jsonify("Failed to delete record from the database"), 500
+
     if count == 0:
-        return "Oops! Person not found"
-    return "The student was deleted", 200
+        return jsonify("Record not found"), 404
+
+    return jsonify(del_id), 200
+
 
 
 def create_election(request):
